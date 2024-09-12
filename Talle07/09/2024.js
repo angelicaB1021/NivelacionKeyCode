2c13bf7ad71c676e3986fa2775e907ea73264c3d
angelicaB1021 <rabeltran3@misena.edu.co>
2024-09-12 15:59:44 -0500

    ..

---
+++ Talle07/09/2024.js
@@ -0,0 +1,9 @@
+let herviros = ['Lombrices', 'Vaquitas', 'Conejos', 'Orujas'];
+let carnivoros = ['Perros', 'Tiburones', 'Gatos', 'Aguilas'];
+let animales = herviros;
+
+if (animales == carnivoros) {
+    console.log('Son animalitos que comen carne');
+}else{
+     console.log('Son animalitos que comen hierbitas');
+}
